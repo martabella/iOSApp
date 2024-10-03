dccfa6c0bfd944010c81365476475ad65608d403
martabella <mbella@lasalle.cat>
2024-10-03 14:43:27 +0200

    Some changes to update the score value

---
+++ iOSApp/Views/MarksView.swift
@@ -0,0 +1,18 @@
+//
+//  MarksView.swift
+//  iOSApp
+//
+//  Created by Marta Bella Gómez on 3/10/24.
+//
+
+import SwiftUI
+
+struct MarksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MarksView()
+}
